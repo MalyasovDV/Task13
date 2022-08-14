062de1ef7771213dffbb5157b37af5c19088705b
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-15 07:19:36 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿void ThirdDigit(int num)
+{
+    int digit = -1;
+    if (num / 100 > 1)
+    {
+        while (num / 100 > 1)
+        {
+            digit = num % 10;
+            num /= 10;
+        }
+        Console.WriteLine(digit);
+    }
+    else 
+        Console.WriteLine("Третьей цифры нет");
+}
+
+int num = 645;
+ThirdDigit(num);
+
+num = 78;
+ThirdDigit(num);
+
+num = 32679;
+ThirdDigit(num);
